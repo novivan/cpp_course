457fc77ddcbd489b215faed1c0deb681b4c50b44
novivan <nov_ivan_777@mail.ru>
2024-09-26 00:57:33 +0300

    added new class iterator in methods for findig min and max keys

--- Task_1/main.cpp
+++ Task_1/main.cpp
@@ -28,6 +28,13 @@
         добавлениe элементов по ключу
     не должен изменять дерево, если элемент с таким ключем уже существует
  
+ //DONE
+        нахождение минимального ключа
+ 
+ //DONE
+        нахождение максимального ключа
+ 
+ 
 //UNDONE
         поиск элементов по ключу
     должен возвращать либо валидный итератор на соответствующий элемент, либо итератор равный end(), если такого элемента нет в контейнере
@@ -36,11 +43,6 @@
         удалениe элементов по ключу
     если ключа нет в дереве, не меняет его
  
- //UNDONE
-        нахождение минимального ключа
- 
- //UNDONE
-        нахождение максимального ключа
  
  //UNDONE
         перечисления элементов контейнера, значения
@@ -52,34 +54,72 @@
  Итераторы должны оставаться валидными
  **/
 
+template <class K, class V>
+class ST_iterator;
+
 
 template<class K, class V>
 class SearchingTree {
-    
-typedef  SearchingTree* ptrST;
-typedef std::pair<ptrST, ptrST> ptrST_2;
-    
+    typedef SearchingTree<K, V> *ptrST;
+    typedef std::pair<ptrST, ptrST> ptrST_2;
 private:
     K k;
     V val;
     ptrST left, right;
-    
 public:
     //FUNCTIONS FROM TASK:
         //DONE:
     SearchingTree(const K &key, const V &value);
     void insert(const K &key, const V &value); //...
-        //UNDONE:
-    void erase(const K &key); //...
-    V find(const K &key); //...
     K min_key();
     K max_key();
+    ST_iterator<K, V> operator& ();
+        //UNDONE:
+    void erase(const K &key); //...
+    /* ??? (на самом деле не V) */ V find(const K &key); //...
     void print_elems(K a, K b);
     
-    //ACCOMPANING FUNCTIONS ()
+    //SUPPORTING FUNCTIONS (NEEDED TO HELP OTHER FUNCTIONS WORK)
+        //UNDONE
     ptrST_2 split(const K &key);
     ptrST merge(ptrST left, ptrST right);
 };
+
+//TODO: increase by 1 (operaor++), constructor(with instance)
+template <class K, class V>
+class ST_iterator{
+public:
+    typedef SearchingTree<K, V> *ptrST;
+    ptrST instance;
+    ST_iterator(ptrST inst) {
+        this->instance = inst;
+    }
+    
+    //TODO: add implementation
+    void operator++() {
+        //TODO: finish. and think about how to kepp root in memory (or kepp parent of each node)
+        /*
+        if (this->instance->right) {
+            this->instance = this->instance->right;
+            while (this->instance->left) this->instance = this->instance->left;
+            return;
+        }*/
+    };
+    
+    SearchingTree<K, V> & operator*() {
+        return *instance;
+    };
+};
+
+
+
+
+
+template <class K, class V>
+ST_iterator<K, V> SearchingTree<K, V>::operator& (){
+    return ST_iterator<K, V>::ST_iterator(this);
+};
+
 
 template <class K, class V>
 SearchingTree<K, V>::SearchingTree(const K &key, const V &value){
@@ -112,6 +152,27 @@ void SearchingTree<K, V>::insert(const K &key, const V &value) {
     this->right = new SearchingTree(key, value);
     return;
 }
+
+
+template <class K, class V>
+K SearchingTree<K, V>::min_key() {
+    if (this == nullptr) { //ТАК НЕ ДОЛЖНО БЫТЬ (проверка на это должна быть перед вызовом)
+        return K();
+    }
+    if (this->left) return this->left->min_key();
+    return this->k;
+}
+
+template <class K, class V>
+K SearchingTree<K, V>::max_key() {
+    if (this == nullptr) { //ТАК НЕ ДОЛЖНО БЫТЬ (проверка на это должна быть перед вызовом)
+        return K();
+    }
+    if (this->right) return this->right->min_key();
+    return this->k;
+}
+
+
 
 
 
